0b91655bd6db390d7b640204beeb39a0b2e344a8
Frédéric Suire <fred_suire@yahoo.fr>
2023-03-31 20:58:46 +0200

    correct buiild i hope

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,24 @@ import esbuild from 'rollup-plugin-esbuild'
 
 const input = {
   index: 'src/index.ts',
+  BoarzElement: 'src/BoarzElement.ts',
+  BoarzFormElement: 'src/BoarzFormElement.ts',
+  Drawer: 'src/Drawer.ts',
+  InputNumber: 'src/InputNumber.ts',
+  Overlay: 'src/Overlay.ts',
+
+  // behaviours
+  'behaviours/index': 'src/behaviours/index.ts',
+  'behaviours/DrawerBehaviour': 'src/behaviours/DrawerBehaviour.ts',
+  'behaviours/OverlayBehaviour': 'src/behaviours/OverlayBehaviour.ts',
 
   // utils
-  // 'utils/index': 'src/utils/index.ts',
+  'utils/index': 'src/utils/index.ts',
   'utils/castToboolean': 'src/utils/castToboolean.ts',
+  'utils/debounce': 'src/utils/debounce.ts',
+  'utils/kebabCaseToCamelCase': 'src/utils/kebabCaseToCamelCase.ts',
+  'utils/kebabCaseToPascalCase': 'src/utils/kebabCaseToPascalCase.ts',
+  'utils/removeLastChars': 'src/utils/removeLastChars.ts',
 }
 
 export default [
