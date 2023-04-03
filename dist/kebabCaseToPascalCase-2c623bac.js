60d1b4b6abbb9490ec6e33785ed0b83c9caadf7b
Frédéric Suire <fred_suire@yahoo.fr>
2023-04-03 20:30:10 +0200

    other things

--- dist/kebabCaseToPascalCase-2c623bac.js
+++
@@ -1,8 +0,0 @@
-function kebabCaseToPascalCase(kebabString) {
-  return kebabString.split("-").reduce((acc, current) => {
-    acc += current.charAt(0).toUpperCase() + current.slice(1);
-    return acc;
-  }, "");
-}
-
-export { kebabCaseToPascalCase as k };
